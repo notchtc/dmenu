50669459f2e484a25b3227fb44cc32a12222815b
chtc <notnotcha0t1c@protonmail.com>
2022-02-03 13:25:06 +0100

    make font bigger and move to nerdfont

--- config.h
+++ config.h
@@ -5,9 +5,9 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka:size=12",
-	"Noto Emoji:size=12",
-	"Noto Emoji Color:size=12"
+	"Iosevka Nerd Font:size=13",
+	"Noto Emoji:size=13",
+	"Noto Emoji Color:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
