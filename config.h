95d04c52859a0608a610df00eb78c96391cce59c
chtc <notnotcha0t1c@protonmail.com>
2022-02-03 12:50:47 +0100

    new build

---
+++ config.h
@@ -0,0 +1,28 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Iosevka:size=12",
+	"Noto Emoji:size=12",
+	"Noto Emoji Color:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#c0caf5", "#1a1b26" },
+	[SchemeSel] = { "#1a1b26", "#f7768e" },
+	[SchemeSelHighlight] = { "#c0caf5", "#f7768e" },
+	[SchemeNormHighlight] = { "#f7768e", "#1a1b26" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
