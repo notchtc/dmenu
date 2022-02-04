d997c70eeb5eeee188348015f6418eaa21ae5e78
chtc <notnotcha0t1c@protonmail.com>
2022-02-04 13:38:40 +0100

    new build

--- config.h
+++ config.h
@@ -2,21 +2,30 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static char font[] = "monospace:size=13";
 static const char *fonts[] = {
-	"Iosevka Nerd Font:size=13",
+	font,
 	"Noto Emoji:size=13",
-	"Noto Emoji Color:size=13"
+	"Noto Color Emoji:size=13"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char fuzzycolor[]  = "#ffc978";
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c0caf5", "#1a1b26" },
-	[SchemeSel] = { "#1a1b26", "#f7768e" },
-	[SchemeSelHighlight] = { "#c0caf5", "#f7768e" },
-	[SchemeNormHighlight] = { "#f7768e", "#1a1b26" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor  },
+	[SchemeSelHighlight] = { fuzzycolor, selbgcolor },
+	[SchemeNormHighlight] = { fuzzycolor, normbgcolor },
+	[SchemeOut]  = { "#000000",   "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
@@ -26,3 +35,16 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",        STRING, &font },
+	{ "normfgcolor", STRING, &normfgcolor },
+	{ "normbgcolor", STRING, &normbgcolor },
+	{ "selfgcolor",  STRING, &selfgcolor },
+	{ "selbgcolor",  STRING, &selbgcolor },
+	{ "fuzzycolor",  STRING, &fuzzycolor },
+	{ "prompt",      STRING, &prompt },
+};
